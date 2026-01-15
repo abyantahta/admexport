379e7c43b9118b73da002a732f512b4f75471c45
abyantahta <abyantahta2002@gmail.com>
2026-01-15 16:32:05 +0700

    asd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'triwanto@gmail.com',
+            'password' => Hash::make('12345678'),
         ]);
         // Interlock::factory()->create([
         //     'isLocked' => false,
